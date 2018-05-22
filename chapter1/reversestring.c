84f20cf03d0a907e548ac1a3b3fe35a57c2de350
your name <yor eMail adress>
2018-05-22 04:23:27 +0200

    reverse a string

---
+++ chapter1/reversestring.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void main()
+{
+	char string[] = "Hello World";
+        int count = 0,index = 0;
+        char*str = (char*)&string;
+        int size = sizeof(string);
+        printf("Size of string %d\n",size);
+        char*ptr = (char*)malloc(size);
+        for(index = size-2 ;index >= 0;index--)
+	{
+		ptr[count] = str[index];
+                count++;
+	}
+        ptr[size-1] = '\0';
+  
+        printf("Input String: %s\n",string);
+
+	printf("Reversed String: %s\n",ptr);
+}
